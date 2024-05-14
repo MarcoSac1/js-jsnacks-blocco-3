378df46c1a6c6831314c2afa90f18f4b4b929ff4
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-14 16:05:29 +0200

    maked Array and control over the elements

---
+++ array-lista/js/script.js
@@ -0,0 +1,10 @@
+// creo un Array con gli elementi che mi sono stati forniti 
+let fridgeFruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
+
+// controllo quali elementi sono presenti al suo interno 
+console.log(fridgeFruits);
+
+// agiungo con un push l'elemento 'pesca'
+fridgeFruits.push('pesca');
+// controllo se e stato agiunto
+console.log(fridgeFruits);
