2bf5611459f9e12efecc83f6a4de55c076752d0d
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-15 12:49:36 +0200

    created empty Array and while cicle

---
+++ snack10 copy/js/script.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+
+const emptyArray = [];
+
+while (emptyArray.length < 2) {
+    let userNumber = Number.parseInt(prompt('type a number') ,10);
+}if(Number.isNaN(userNumber)){
+    arrayElement.push(userNumber);
+    
+}
+console.log(emptyArray.length);
+
