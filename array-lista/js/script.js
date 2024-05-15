4a4ae456ae73ce858003a87bdb8395bd211b0d2f
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-15 14:38:54 +0200

    fixed code issue

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -2,27 +2,29 @@
 let fridgeFruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
 
 // controllo quali elementi sono presenti al suo interno 
-console.log(fridgeFruits);
+// console.log(fridgeFruits);
 
 // agiungo con un push l'elemento 'pesca'
 fridgeFruits.push('pesca');
 
 // controllo se e stato agiunto
-console.log(fridgeFruits);
+// console.log(fridgeFruits);
+
+let isFound = false;
 
 // controllo se dentro l'Array è presente 'cocomero'
 for (let index = 0; index < fridgeFruits.length; index++) {
-    const element = fridgeFruits[index];
-    console.log(element);
-    
-    if(element ==='cocomero'){
-        console.log('Trovato! Devo solo preparare il cocktail.');
-    }else{
-        console.log("Oh no, devo uscire a comprare il cocomero!");
+    if (fridgeFruits[index] === 'cocomero'){
+        isFound = true;
     }
-    
 }
 
+// creo un controllo
+if(isFound){
+    console.log('Trovato! Devo solo preparare il cocktail.');
+}else{
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
 
 // const iterator = fridgeFruits.values()
 
