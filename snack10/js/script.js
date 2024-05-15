0cfe6fd592f966eb15afa5219a9da4a79f511669
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-15 12:34:53 +0200

    fixed code issue

--- snack10/js/script.js
+++ snack10/js/script.js
@@ -1,14 +1,11 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-let firstArray = [1, 2, 3, 4, 5, 6,];
-let secondArray = [7, 8, 9, ];
+const firstArray = [7, 8, 9];
+const secondArray = [1, 2, 3, 4, 5, 6];
 
 while (firstArray.length < secondArray.length){
-    firstArrayArray.push('7,');
-    console.log(firstArray);
-
-}if(firstArray > secondArray){
-    secondArray.push('10,');
-    console.log(secondArray);
+    firstArray.push('7,');
+    
 }
+console.log(firstArray, secondArray);
