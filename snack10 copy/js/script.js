784a3d7541613a37d24d46ef49237476026553e2
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-15 12:59:47 +0200

    fixed code issue

--- snack10 copy/js/script.js
+++ snack10 copy/js/script.js
@@ -4,11 +4,20 @@
 
 const emptyArray = [];
 
-while (emptyArray.length < 2) {
-    let userNumber = Number.parseInt(prompt('type a number') ,10);
-}if(Number.isNaN(userNumber)){
-    arrayElement.push(userNumber);
+let  somma = 0;
+
+while(somma < 50) {
+    let userNumber = Number.parseInt(prompt('type a number'), 10);
     
+    if (Number.isNaN(userNumber) !== true) {
+        userNumber = Number. parseInt(prompt('type a number'), 10);
+        emptyArray.push(userNumber);
+        somma += userNumber;
+    }
 }
-console.log(emptyArray.length);
+
+console.log(emptyArray, somma);
+    
+
+
 
