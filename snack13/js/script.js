b398484ffea980baa937469e090ef0578958ddd2
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-16 11:55:22 +0200

    created snack13 folder

---
+++ snack13/js/script.js
@@ -0,0 +1,14 @@
+// Snack9 
+// Crea un array vuoto. Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
+//  se è dispari inseriscilo nell'array.
+// const emptyArray = [];
+
+
+// while (emptyArray.length < 6){
+//     let userNumber = Number.parseInt(prompt('type a number'), 10);
+// }if(Number.isNaN(userNumber) !== true && userNumber % 2 === 1){
+//     arrayElement.push(userNumber);
+
+// }
+
+// console.log(emptyArray);
