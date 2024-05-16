72b1958fa31412ded5f04168fa8eacc4f9b5a27c
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-16 12:08:01 +0200

    created variable, for loop and function base

---
+++ snack13/js/script.js
@@ -0,0 +1,13 @@
+// > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
+
+const number = parseInt(prompt('type a number'), 10);
+
+for (let index = 0; index < number.length; index++) {
+    const number = array[index];
+    
+}
+
+function name(params) {
+    
+}
+
