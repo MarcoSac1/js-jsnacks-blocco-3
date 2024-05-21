e76774cf8403299facbcb73ac3a79dc4253f34fe
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-21 12:41:00 +0200

    created timer

--- snack14/js/script.js
+++ snack14/js/script.js
@@ -1,3 +1,20 @@
+// ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
+
+// chiedo al utente di inserire un numero
+let usernumber = Number.parseInt(prompt('type a number'), 10);
+
+// se l'utente ha inserito un numero troppo lungo lo sovrascrivo con 5 secondi
+if(usernumber > 10 || Number.isNaN(usernumber)){
+    usernumber = 5;
+}
 
 
-
+const timer = setInterval(function(){
+    usernumber--;
+    console.log(usernumber);
+    if(timer <= 0){
+        clearInterval(timer);
+        alert('no more time');
+    }
+    
+}, 2000);
