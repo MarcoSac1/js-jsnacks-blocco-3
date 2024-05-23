e666cf2d35cc0fc2aefa4f2ed95a065300e24a07
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-23 12:14:55 +0200

    created  array and forEach loop

---
+++ snackObject1/js/script.js
@@ -0,0 +1,87 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+
+const automobili = [
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'benzina',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'benzina',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'benzina',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'benzina',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'benzina',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'diesel',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'diesel',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'diesel',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'diesel',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'diesel',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'metano',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'metano',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'metano',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'elettrico',
+    },
+    {
+        marca : 'fiat',
+        modello :'panda',
+        alimentazione : 'elettrico',
+    },
+];
+
+automobili.forEach((element) =>{
+    console.log(element);
+})
