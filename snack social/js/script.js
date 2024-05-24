cbb1ec1c295f6360451c90a1e30a5533b2fa18a9
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-24 11:42:50 +0200

    created snack folder

---
+++ snack social/js/script.js
@@ -0,0 +1,79 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+// Dividi in due array separati le zucchine che misurano meno o più di 8cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+const zucchine = [
+    {
+        varieta : 'bianca',
+        peso : '46',
+        lunghezza :'3',
+    },
+    {
+        varieta : 'bianca',
+        peso : '542',
+        lunghezza : '165',
+    },
+    {
+        varieta : 'bianca',
+        peso : '860',
+        lunghezza : '35',
+    },
+    {
+        varieta : 'zucchina chiara di faenza',
+        peso : '20',
+        lunghezza : '2',
+    },
+    {
+        varieta : 'bianca',
+        peso : '505',
+        lunghezza : '215',
+    },
+    {
+        varieta : 'zucchina chiara di faenza',
+        peso : '37',
+        lunghezza : '200',
+    },
+    {
+        varieta : 'bianca',
+        peso : '33',
+        lunghezza : '7',
+    },
+    {
+        varieta : 'zucchina chiara di faenza',
+        peso : '60',
+        lunghezza :'6',
+    },
+    {
+        varieta : 'zucchina chiara di faenza',
+        peso : '520',
+        lunghezza : '11',
+    },
+    {
+        varieta : 'bianca',
+        peso : '20',
+        lunghezza : '5',
+    },
+];
+
+zucchine.forEach((zucchina) =>) {
+    weigt += Number.parseInt(zucchina.peso);
+}
+console.log(weigt);
+
+let zucchinaCorta = zucchine.filter((element) =>{
+    if(element.lunghezza < 8){
+        return true;
+    }
+});
+
+console.log(zucchinaCorta);
+
+
+let zucchinaLunga = zucchine.filter((element) =>{
+    if(element.lunghezza > 8){
+        return true;
+    }
+});
+
+console.log(zucchinaLunga);
