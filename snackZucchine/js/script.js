b5b6bb57485d99acac5e5e358de013f54d3974a9
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-24 11:14:24 +0200

    created snack folder

---
+++ snackZucchine/js/script.js
@@ -0,0 +1,4 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+// Dividi in due array separati le zucchine che misurano meno o più di 8cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
